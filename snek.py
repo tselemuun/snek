1c59f4f0fcb6daf5c93acc2f774795ae087afbbd
tselemuun <tselemuun@gmail.com>
2026-01-21 14:52:58 +0800

    Update snek.py

--- snek.py
+++ snek.py
@@ -8,6 +8,11 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * ROWS
 WINDOW_HEIGHT = TILE_SIZE * COLS
 
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 #game window
 window = tkinter.Tk()
 window.title("Snak")
@@ -28,4 +33,50 @@ window_y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) 
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+velocityX = 0
+velocityY = 0
+
+def change_direction(e):
+    #print(e)
+    #print(e.keysym)
+    global velocityX, velocityY
+
+    if(e.keysym == "Up" and velocityY != 1):
+        velocityX = 0
+        velocityY = -1
+    elif(e.keysym == "Down" and velocityY != -1):
+        velocityX = 0
+        velocityY = +1
+    elif(e.keysym == "Left" and velocityX != 1):
+        velocityX = -1
+        velocityY = 0
+    elif(e.keysym == "Right" and velocityX != -1):
+        velocityX = +1
+        velocityY = 0
+
+def move():
+    global snake
+
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
+
+def draw():
+    global snake
+    move()
+
+    canvas.delete("all")
+
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "white")
+
+    #mogoi zurn
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "pink")
+
+
+    window.after(100, draw) #10fps tei zurn
+
+draw()
+
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
