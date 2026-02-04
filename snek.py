ebe5ec98554af7f94716ae97a7afdd405760e901
Tseku <tselemuun@gmail.com>
2026-02-04 09:29:35 +0800

    Update snek.py

--- snek.py
+++ snek.py
@@ -38,11 +38,15 @@ food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
 snake_body = []
 velocityX = 0
 velocityY = 0
+game_over = false
+score = 0
 
 def change_direction(e):
     #print(e)
     #print(e.keysym)
     global velocityX, velocityY
+    if(game_over):
+        return
 
     if(e.keysym == "Up" and velocityY != 1):
         velocityX = 0
@@ -58,18 +62,41 @@ def change_direction(e):
         velocityY = 0
 
 def move():
-    global snake
+    global snake, food, snake_body, game_over
+    if(game_over):
+        return
+
+    if (snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >= WINDOW_HEIGHT):
+        game_over  = true
+        return
+
+    for tile in snake_body:
+        if(snake.x == tile.x and snake.y == tile.y):
+            game_over = true
+            return
 
     if(snake.x == food.x and snake.y == food.y):
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLS-1) * TILE_SIZE
         food.y = random.randint(0, ROWS-1) * TILE_SIZE
+        score += 1
+
+    #update snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
 
     snake.x += velocityX * TILE_SIZE
     snake.y += velocityY * TILE_SIZE
 
 def draw():
-    global snake
+    global snake, food, snake_body, game_over, score
     move()
 
     canvas.delete("all")
@@ -81,6 +108,12 @@ def draw():
 
     for tile in snake_body:
         canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill = "pink")
+
+    if (game_over):
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, font = "Arial 20", text = f"Game Over: {score}", fill = "white")
+
+    else:
+        canvas.create_text(30, 20, font = "Arial 10", text = f"Score: {score}", fill = "white")
 
     window.after(100, draw) #10fps tei zurn
 
